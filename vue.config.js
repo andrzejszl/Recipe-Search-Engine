dc6a9ff332bf09b10270d6ff01767c6e23b74682
andrzejszl <andrzej.sz123@gmail.com>
2022-12-02 04:15:37 +0100

    add missing dependencie minicssextraplugin

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  }
+};
